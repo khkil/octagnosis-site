9ff5630a54a51152a2e1ec8bbd7ca8e8ef77b9b6
khkil <khkil@micehub.com>
2021-11-25 17:58:09 +0900

    기관상세 ui 구현, groupReducer 로직 수정

--- src/pages/admin/group/AdminGroupDetail.js
+++ src/pages/admin/group/AdminGroupDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/macro";
-import { NavLink } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik } from "formik";
 
 import Helmet from "react-helmet";
 import { withStyles } from '@material-ui/core/styles';
@@ -9,7 +10,10 @@ import {
   Button,
   Grid,
   Paper as MuiPaper,
-  Divider
+  Divider,
+  Card,
+  CardContent,
+  TextField
  
 } from "@material-ui/core";
 
@@ -25,18 +29,29 @@ import {
 import { spacing } from "@material-ui/system";
 import { useDispatch, useSelector } from "react-redux";
 import MenuBar from "../../../components/MenuBar";
+import { getAdminGroupDetail } from "../../../redux/actions/groupActions";
 
 const Paper = styled(MuiPaper)(spacing);
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("기관명을 입력하세요"),
+});
 
 const AdminGroupDetail = ({ match }) => {
   
   const dispatch = useDispatch();
+  const { selected } = useSelector(state => state.groupReducer);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   useEffect(() => {
-    
+    const groupIdx = match.params.idx;
+    dispatch(getAdminGroupDetail(groupIdx));
   }, []);
 
   
+  if(!selected) return null;
   return (
     <React.Fragment>
       <Helmet title="회원 목록" />
@@ -46,20 +61,108 @@ const AdminGroupDetail = ({ match }) => {
         <Grid item>
           <Button variant="contained" color="primary">
             <AddIcon />
-            New Order
+            기관 추가
           </Button>
         </Grid>
       </Grid>
       <Divider my={6} />
 
-      <Grid container spacing={6}>
-        <Grid item xs={12}>
-          <Paper>
-       
-          </Paper>
-
-        </Grid>
-      </Grid>
+      <Card mb={6}>
+        <CardContent>
+          <Formik
+            initialValues={selected}
+            validationSchema={validationSchema}
+          >
+            {({
+              errors,
+              handleBlur,
+              handleChange,
+              isSubmitting,
+              touched,
+              values,
+              status,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                {JSON.stringify(selected)}
+                {JSON.stringify(touched)}
+                <Grid container spacing={6} m={5}>
+                  <Grid item md={6}>
+                    <TextField
+                      name="name"
+                      label="기관명"
+                      value={values.name}
+                      error={Boolean(touched.name && errors.name)}
+                      fullWidth
+                      helperText={touched.name && errors.name}
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      variant="outlined"
+                      my={2}
+                    />
+                  </Grid>
+                  <Grid item md={6}>
+                    <TextField
+                      name="lastName"
+                      label="Last Name"
+                      value={values.lastName}
+                      error={Boolean(touched.lastName && errors.lastName)}
+                      fullWidth
+                      helperText={touched.lastName && errors.lastName}
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      variant="outlined"
+                      my={2}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid container spacing={6} m={5}>
+                  <Grid item md={6}>
+                    <TextField
+                      name="firstName"
+                      label="First Name"
+                      value={values.firstName}
+                      error={Boolean(touched.firstName && errors.firstName)}
+                      fullWidth
+                      helperText={touched.firstName && errors.firstName}
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      variant="outlined"
+                      my={2}
+                    />
+                  </Grid>
+                  <Grid item md={6}>
+                    <TextField
+                      name="lastName"
+                      label="Last Name"
+                      value={values.lastName}
+                      error={Boolean(touched.lastName && errors.lastName)}
+                      fullWidth
+                      helperText={touched.lastName && errors.lastName}
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      variant="outlined"
+                      my={2}
+                    />
+                  </Grid>
+                </Grid>
+                <Grid container spacing={6} m={5}>
+                  <Grid item md={6}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    mt={3}
+                  >
+                    수정
+                  </Button>
+                  </Grid>
+                </Grid>
+              
+              </form>
+            )}
+          </Formik>
+        </CardContent>
+      </Card>
     </React.Fragment>
   );
 }
